e57031feae4596db2f6bbd4381d8dd8deeca76d3
Luke Urbanski <conantheking101@yahoo.com>
2019-02-21 02:04:15 +0000

    Add a route that renders a view for one specific goshuin.

--- routes/goshuins.js
+++ routes/goshuins.js
@@ -2,13 +2,25 @@ var express             = require("express"),
     router              = express.Router(),
     Goshuin             = require("../models/goshuin");
 
+
+// Main goshuin index route
 router.get("/goshuins", function(req, res){
     Goshuin.find({}, function(err, foundGoshuins){
         if(err){
             console.log(err);
         } else {
-            res.render("goshuins/index.ejs", {goshuins: foundGoshuins});
+            res.render("goshuins/index", {goshuins: foundGoshuins});
         }
+    });
+});
+
+// Show a specific goshuin route
+router.get("/goshuins/:id", function(req, res){
+    Goshuin.findById(req.params.id, function(err, foundGoshuin){
+        if(err){
+            console.log(err);
+        } 
+        res.render("goshuins/show", {goshuin: foundGoshuin});
     });
 });
 
