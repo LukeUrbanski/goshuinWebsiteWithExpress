ce4245374493ce5d7ba9be109b95ee199e5b642e
Luke Urbanski <conantheking101@yahoo.com>
2019-03-05 15:05:02 +0000

    Require passport related models and configure app.js to use passport with the local strategy.

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 // Load requirements
-var express             = require("express"),
-    app                 = express(),
-    mongoose            = require("mongoose"),
-    seedDB              = require ("./seed.js"),
-    methodOverride      = require("method-override"),
-    bodyParser          = require("body-parser");
+var express                 = require("express"),
+    app                     = express(),
+    mongoose                = require("mongoose"),
+    seedDB                  = require ("./seed.js"),
+    methodOverride          = require("method-override"),
+    bodyParser              = require("body-parser"),
+    expressSession          = require("express-session"),
+    passport                = require("passport"),
+    LocalStrategy           = require("passport-local");
     
 // Require models
-var Goshuin = require("./models/goshuin.js");
+const Goshuin = require("./models/goshuin.js");
+const User = require("./models/user.js");
     
 // Connect to the database
 mongoose.connect('mongodb://localhost:27017/goshuin', {useNewUrlParser: true});
@@ -19,11 +23,38 @@ seedDB();
 var indexRoutes         = require("./routes/index.js"),
     goshuinRoutes       = require("./routes/goshuins.js");
 
+
+
 // General app settings
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(methodOverride('_method'));
+
+// Authentication general settings
+app.use(expressSession({
+  secret: "1t3snsvnvosfinvosfvn3294nr3",
+  resave: false,
+  saveUninitialized: true
+}));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Passport strategy configuration
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+    ));
 
 // Utilize route variables
 app.use(indexRoutes);
