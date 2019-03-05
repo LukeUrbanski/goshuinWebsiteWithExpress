7c5e365352cd0a0335aed1110b991ca10fe9dc4a
Luke Urbanski <conantheking101@yahoo.com>
2019-03-05 17:36:14 +0000

    Edit the app configurations to better suit the passport-local-mongoose package.

--- app.js
+++ app.js
@@ -39,22 +39,10 @@ app.use(expressSession({
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(passport.initialize());
 app.use(passport.session());
-
 // Passport strategy configuration
-passport.use(new LocalStrategy(
-    function(username, password, done) {
-    User.findOne({ username: username }, function (err, user) {
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-      return done(null, user);
-    });
-  }
-    ));
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // Utilize route variables
 app.use(indexRoutes);
