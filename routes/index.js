5edad4654b89fb84592ed5e81b2b0358939b0d06
Luke Urbanski <conantheking101@yahoo.com>
2019-03-05 15:13:00 +0000

    Add a post route that will create a new MongoDB document in the users collection.

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,28 @@
 // Load requirements
 var express         = require("express"),
-    router          = express.Router();
+    router          = express.Router(),
+    User            = require("../models/user.js");
 
 //Landing page route    
 router.get("/", function(req, res){
     res.render("landing");
-});    
+});
+
+// Create user form
+router.get("/user/register", function(req, res){
+    res.render("register");
+})
+
+// Create a new user
+router.post("/user", function(req, res){
+    User.register({username: req.body.newUser.username}, req.body.newUser.password, function(err, newUser){
+        if(err){
+            console.log(err);
+        } else {
+            console.log(newUser);
+        }
+    });
+});
 
 //Export the routes
 module.exports = router;
