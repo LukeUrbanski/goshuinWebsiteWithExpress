94da194805938e9ad44bcd3d2b12b3214ef6485c
Luke Urbanski <conantheking101@yahoo.com>
2019-03-11 09:56:52 +0000

    Add a new schema key that will store the value of a user's Facebook ID.

--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const passportLocalMongoose   = require("passport-local-mongoose");
 
 var userSchema = mongoose.Schema({
+    facebookId: Number,
     username: String,
     password: String
 });
