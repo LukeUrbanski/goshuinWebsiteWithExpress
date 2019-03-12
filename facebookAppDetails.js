9130335f9685110c87002773a1137849532ba6ba
Luke Urbanski <conantheking101@yahoo.com>
2019-03-12 04:38:25 +0000

    Create an external.js file that would export the required Facebook app details.

---
+++ facebookAppDetails.js
@@ -0,0 +1,15 @@
+// This file is for storing the app details required by Facebook in order 
+// to use the passport-facebook node package. An app ID and app secret 
+// provided by Facebook are required and your own URL is required. Please 
+// set up an app and then read the documents at Facebook Developer for 
+// more information here: see https://developers.facebook.com/
+// After you have recieved your app ID and secret, enter them in the fields
+// below and use your URL.
+
+var facebookAppDetails = {
+    facebookAppId: "YOUR APP ID HERE",
+    facebookAppSecret: "YOUR APP PASSWORD HERE",
+    facebookAppURL: "YOUR URL HERE"
+};
+
+module.exports = facebookAppDetails;
