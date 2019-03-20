4a09d0ae862260a535dfe4bde2e0631e1356833a
Luke Urbanski <conantheking101@yahoo.com>
2019-03-20 07:11:29 +0000

    Add a post route that will create a comment and a reference to it in a goshuin document.

--- routes/comments.js
+++ routes/comments.js
@@ -1,7 +1,8 @@
 // Requirements
 var express     = require("express"),
     router      = express.Router(),
-    Goshuin     = require("../models/goshuin");
+    Goshuin     = require("../models/goshuin"),
+    Comment     = require("../models/comments");
 
 
 // =================================
@@ -17,5 +18,34 @@ router.get("/goshuins/:id/comments/new", function(req, res){
         }
     });
 });
+
+// Post the new comment by finding the relevant goshuin,
+// creating a comment and then adding it to the relevant goshuin array
+router.post("/goshuins/:id/comments/", function(req, res){
+    // Store the posted data into a variable
+    var postedComment = {
+       comment: req.body.comment,
+   }
+   // Find the relevant goshuin
+   Goshuin.findById(req.params.id, function(err, foundGoshuin){
+       if(err){
+           console.log(err);
+           res.redirect("/");
+       } else {
+           // Create the comment with the posted data
+           Comment.create(postedComment, function(err, newComment){
+               if(err){
+                   console.log(err);
+                   res.redirec("/");
+               } else {
+                // Add the comment to the goshuin document array   
+                foundGoshuin.userComments.push(newComment);
+                foundGoshuin.save();
+                res.redirect("/goshuins/" + foundGoshuin._id);
+               }
+           })
+       }
+   })
+})
 
 module.exports = router;
