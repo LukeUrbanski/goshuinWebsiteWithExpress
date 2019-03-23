931bae3931162d1d9d141e8a721b68f11d5d7390
Luke Urbanski <conantheking101@yahoo.com>
2019-03-23 10:18:10 +0000

    Add a key value pair with a Date object.

--- models/comments.js
+++ models/comments.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 var commentSchema = mongoose.Schema({
     commentAuthor: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
-    comment: String
+    comment: String,
+    date: {type: Date, default: Date}
 });
 
 module.exports = mongoose.model("Comment", commentSchema);
